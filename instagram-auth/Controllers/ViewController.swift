498db4f778650d195f4624e8c2d27bde322d1e1f
Nelida Velazquez <tolkiana@gmail.com>
2016-06-12 20:05:55 -0400

    First commit, creating folders and creating login button

---
+++ instagram-auth/Controllers/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  instagram-auth
+//
+//  Created by Nelida Velazquez on 6/12/16.
+//  Copyright © 2016 Tolkiana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func login(sender: AnyObject) {
+    }
+}
+
