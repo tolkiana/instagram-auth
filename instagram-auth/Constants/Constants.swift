fb0c5495854f6a37fcea69fa5131d28c09bfc0bd
Nelida Velazquez <tolkiana@gmail.com>
2016-06-12 20:45:39 -0400

    Adding more constants

--- instagram-auth/Constants/Constants.swift
+++ instagram-auth/Constants/Constants.swift
@@ -11,7 +11,7 @@ import Foundation
 enum Constans {
     
     enum URL {
-        static let instagramAPI = "https://api.instagram.com"
+        static let instagramAPI = "https://api.instagram.com/"
         static let redirectURI = "http://tolkianaa.blogspot.com/"
     }
     
@@ -19,6 +19,23 @@ enum Constans {
         static let clientID = "71d7d343ec85404d980b1b3566ec147e"
         static let clientSecret = "9c31e7395668457fa1d8e160a1a99b22"
         static let grantType = "authorization_code"
+    }
+    
+    enum API {
+        static let authorize = "oauth/authorize"
+        static let accessToken = "oauth/access_token"
+    }
+    
+    enum Key {
+        static let client_id = "client_id"
+        static let redirect_uri = "redirect_uri"
+        static let response_type = "response_type"
+        static let code = "code"
+        static let client_secret = "client_secret"
+        static let grant_type = "grant_type"
+        static let error = "error"
+        static let error_reason = "error_reason"
+        static let error_description = "error_description"
     }
     
     enum ContentType {
