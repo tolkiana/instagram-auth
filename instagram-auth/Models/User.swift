b1145d16109d7081ed31207056ee1c2016f69de0
nelidaVelazquez <nelida.velazquez@dominos.com>
2016-07-04 18:12:31 -0400

    Implementing Parsable protocol

--- instagram-auth/Models/User.swift
+++ instagram-auth/Models/User.swift
@@ -12,12 +12,40 @@ struct User {
     var id: String
     var userName: String
     var fullName: String
-    var pictureURL: NSURL
+    var pictureURL: NSURL?
     
-    init(id: String, userName: String, fullName: String, pictureURL: NSURL) {
+    init(id: String, userName: String, fullName: String, pictureURL: NSURL?) {
         self.id = id
         self.userName = userName
         self.fullName = fullName
         self.pictureURL = pictureURL
+    }
+}
+
+extension User: Parsable {
+    
+    func parse(dictionary: [String : AnyObject]) -> User? {
+        guard let userDictionary = dictionary["user"] else {
+            return nil
+        }
+        
+        guard let id = userDictionary["id"] as? String,
+            let userName = userDictionary["username"] as? String,
+            let fullName = userDictionary["full_name"] as? String else {
+                return nil
+        }
+        
+        guard let pictureString = userDictionary["profile_picture"] as? String,
+            let pictureURL = NSURL(string: pictureString) else {
+                return User(id: id,
+                            userName: userName,
+                            fullName: fullName,
+                            pictureURL: nil)
+        }
+        
+        return User(id: id,
+                    userName: userName,
+                    fullName: fullName,
+                    pictureURL: pictureURL)
     }
 }
