5f2b6a13ce51afd5f115288491e74dc821d751fa
nelidaVelazquez <nelida.velazquez@dominos.com>
2016-07-04 17:36:32 -0400

    Creates Instagram Service with empty methods

---
+++ instagram-auth/Service/InstagramService.swift
@@ -0,0 +1,20 @@
+//
+//  InstagramService.swift
+//  instagram-auth
+//
+//  Created by Nelida Velazquez on 7/4/16.
+//  Copyright © 2016 Tolkiana. All rights reserved.
+//
+
+import Foundation
+
+class InstagramService {
+    
+    func startAuthorization(with url: NSURL) {
+    
+    }
+    
+    func requestAccessToken(with code: String) {
+        
+    }
+}
