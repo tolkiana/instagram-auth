f47972810c174f5ad50f5a9ec28a196e4ee5b971
nelidaVelazquez <nelida.velazquez@dominos.com>
2016-07-04 17:16:56 -0400

    Implementing WebView to load the signin screen

--- instagram-auth/Controllers/LoginViewController.swift
+++ instagram-auth/Controllers/LoginViewController.swift
@@ -10,8 +10,33 @@ import UIKit
 
 class LoginViewController: UIViewController {
     @IBOutlet var webView: UIWebView!
+    @IBOutlet var loadingView: UIActivityIndicatorView!
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        webView.loadRequest(Request.Instagram.authorize())
+    }
+    
     @IBAction func cancel(sender: UIButton) {
         self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+}
+
+extension LoginViewController: UIWebViewDelegate {
+
+    func webViewDidStartLoad(webView: UIWebView) {
+        loadingView.startAnimating()
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        loadingView.stopAnimating()
+        loadingView.removeFromSuperview()
+    }
+    
+    func webView(webView: UIWebView,
+                 shouldStartLoadWithRequest request: NSURLRequest,
+                                            navigationType: UIWebViewNavigationType) -> Bool {
+        return true
     }
 }
