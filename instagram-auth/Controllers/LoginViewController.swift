224e9b9c35ebbabfc4c215771038fcdf1599c31e
Nelida Velazquez <tolkiana@gmail.com>
2016-07-07 22:51:48 -0400

    Call instagram service and implement delegate

--- instagram-auth/Controllers/LoginViewController.swift
+++ instagram-auth/Controllers/LoginViewController.swift
@@ -11,9 +11,12 @@ import UIKit
 class LoginViewController: UIViewController {
     @IBOutlet var webView: UIWebView!
     @IBOutlet var loadingView: UIActivityIndicatorView!
+    
+    let instagramService = InstagramService()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        instagramService.delegate = self
         webView.loadRequest(Request.Instagram.authorize())
     }
     
@@ -38,6 +41,32 @@ extension LoginViewController: UIWebViewDelegate {
     func webView(webView: UIWebView,
                  shouldStartLoadWithRequest request: NSURLRequest,
                                             navigationType: UIWebViewNavigationType) -> Bool {
+        
+        guard let url = request.URL else {
+            return true
+        }
+        
+        instagramService.startAuthorization(withURL: url)
         return true
     }
+}
+
+extension LoginViewController: InstagramDelegate {
+
+    func didGetAuthorizationCode(code: String) {
+        instagramService.requestAccessToken(withCode: code)
+    }
+    
+    func didGetAccessToken(token: String, forUser user: User) {
+        print(token)
+    }
+    
+    func didFailAuthorizing() {
+        print("Fail")
+    }
+    
+    func didFailGettingAccessToken() {
+        print("Fail")
+    }
+
 }
