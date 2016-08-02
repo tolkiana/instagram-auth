71e3981397efce1fb9a2a454f62088975a1686b6
Nelida Velazquez <tolkiana@gmail.com>
2016-08-01 22:59:59 -0400

    Creates dynamic struct to be able to bind viewModels

---
+++ instagram-auth/Models/Dynamic.swift
@@ -0,0 +1,28 @@
+//
+//  Dynamic.swift
+//  instagram-auth
+//
+//  Created by Nelida Velazquez on 8/1/16.
+//  Copyright © 2016 Tolkiana. All rights reserved.
+//
+
+import Foundation
+
+struct Dynamic<T> {
+    typealias Listener = T -> Void
+    var listener: Listener?
+
+    init(_ value: T){
+        self.value = value
+    }
+    
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    mutating func bind(listener: Listener?) {
+        self.listener = listener
+    }
+}
